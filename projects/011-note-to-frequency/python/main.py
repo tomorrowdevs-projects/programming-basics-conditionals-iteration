026fdc1038b4343d12b5b91c139e9c9c13278b81
Daniele <danieleslvstrn@gmail.com>
2023-08-04 15:14:22 +0200

    solution-M1-11

---
+++ projects/011-note-to-frequency/python/main.py
@@ -0,0 +1,25 @@
+user_input = input('Type a note followed by the octave: ').lower()
+
+for i in user_input:
+    if i.isalpha():
+        note = i
+    if i.isdigit():
+        octave = int(i)
+
+
+if note == 'c':
+    frequency = 261.63 / 2**(4-octave)
+elif note == 'd':
+    frequency = 293.66 / 2**(4-octave)
+elif note == 'e':
+    frequency = 329.63 / 2**(4-octave)
+elif note == 'f':
+    frequency = 349.23 / 2**(4-octave)
+elif note == 'g':
+    frequency = 392.00 / 2**(4-octave)
+elif note == 'a':
+    frequency = 440.00 / 2**(4-octave)
+elif note == 'b':
+    frequency = 493.88 / 2**(4-octave)
+
+print(frequency)
