d37495ba1628ebdab2570ffc4bb3682da6ed4d12
AlessandroVerzella <116301354+AlessandroVerzella@users.noreply.github.com>
2023-08-08 18:15:03 +0200

    Entered comment and the solution of the exercise

--- projects/010-month-name-to-number-of-days/js/index.js
+++ projects/010-month-name-to-number-of-days/js/index.js
@@ -1,1 +1,40 @@
 'use strict';
+/*Month Name to Number of Days
+The length of a month varies from 28 to 31 days.
+
+1) In this exercise you will create a program that reads the name of a month from the user as a string.
+2)Then your program should display the number of days in that month.
+3) Display 28 or 29 days for February so that leap years are addressed.
+
+Example:
+Input = April
+Output = 30*/
+
+// I used the .toLowerCase() method to convert the prompt to LowerCase
+const month = prompt(`Please enter the name of a month`).toLowerCase();
+
+if (
+    month === 'april' ||
+    month === 'june' ||
+    month === 'september' ||
+    month === 'november'
+) {
+    alert(`Input = ${month}
+          Output = 30 days`);
+} else if (month === 'february') {
+    alert(`Input = ${month}
+          Output = 28 or 29 days`);
+} else if (
+    month === 'january' ||
+    month === 'march' ||
+    month === 'may' ||
+    month === 'july' ||
+    month === 'august' ||
+    month === 'october' ||
+    month === 'december'
+) {
+    alert(`Input = ${month}
+          Output = 31 days`);
+} else {
+    alert(`The data entered is incorrect. Please enter the name of a month.`);
+}
