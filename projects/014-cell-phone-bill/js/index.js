bf48fda96d19fda6a662a2d074e4fe2aac1ee722
AlessandroVerzella <116301354+AlessandroVerzella@users.noreply.github.com>
2023-08-13 16:49:19 +0200

    added use strict

--- projects/014-cell-phone-bill/js/index.js
+++ projects/014-cell-phone-bill/js/index.js
@@ -1,20 +1,4 @@
 'use strict';
-/* A particular cell phone plan includes for € 15.00 a month:
-
-50 minutes of air time
-50 text messages
-Each additional minute of air time costs € 0.25
-Each additional text messages cost € 0.15
-All cell phone bills include an additional charge of € 0.44 to support 911 call centers
-The entire bill (including the 911 charge) is subject to 5 percent sales tax.
-
-1) Write a program that reads the number of minutes and text messages used in a month from the user.
-
-2) Only display the additional minute and text message charges if the user incurred costs in these categories. 
-
-3) Ensure that all the charges are displayed using 2 decimal places.
-
-*/
 const baseCharge = 15.0;
 const minutesAirTime = 50;
 const textMessages = 50;
