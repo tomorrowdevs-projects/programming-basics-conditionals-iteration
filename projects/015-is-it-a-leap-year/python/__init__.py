7dff06967463a818a37561148522120fcfa24bea
Matteo Muceli <124149722+Noeddashep@users.noreply.github.com>
2023-08-14 18:54:46 +0100

    Update __init__.py

--- projects/015-is-it-a-leap-year/python/__init__.py
+++ projects/015-is-it-a-leap-year/python/__init__.py
@@ -1,10 +1,1 @@
-user_year = int(input('Enter a year:\n'))
 
-if user_year % 400 == 0:
-    print(user_year, 'is a leap year')
-elif user_year % 100 == 0:
-    print(user_year, 'is not a leap year')
-elif user_year % 4 == 0:
-    print(user_year, 'is a leap year')
-else:
-    print(user_year, 'is not a leap year')
