de97909417582549e3877c5d4093ba41b3d85fe4
AlessandroVerzella <116301354+AlessandroVerzella@users.noreply.github.com>
2023-08-17 18:34:29 +0200

    added solution

--- projects/019-average/js/index.js
+++ projects/019-average/js/index.js
@@ -1,1 +1,31 @@
 'use strict';
+
+let sum = 0;
+let count = 0;
+let value;
+
+do {
+    value = parseFloat(
+        prompt(
+            `Enter one number at a time and enter 0 if you want to interrupt the number sequence.`
+        )
+    );
+
+    if (value === 0) {
+        if (count === 0) {
+            alert(`Error: The first value could not be 0`);
+        } else {
+            break;
+        }
+    }
+
+    sum += value;
+    count++;
+} while (true);
+
+if (count === 0) {
+    `No value was entered`;
+} else {
+    const average = sum / count;
+    alert(`(Average of entered values: ${average.toFixed(2)}`);
+}
