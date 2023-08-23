da3aeb6e4e1e63477ecbd52b4c463170c85dca27
Matteo Muceli <124149722+Noeddashep@users.noreply.github.com>
2023-08-23 20:51:18 +0100

    Update main.py

--- projects/027-the-collatz-conjecture/python/main.py
+++ projects/027-the-collatz-conjecture/python/main.py
@@ -1,8 +1,1 @@
-q = int(input('Enter decimal number:\n'))
-result = ''
 
-while q != 0:
-    r = q % 2
-    result = str(r) + result
-    q = q // 2
-print(f'The binary number corresponding is: {result}')
