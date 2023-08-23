da90eb7cda8773b5442fe625545bf2f47aa1c832
Noeddasheep <muceteo@gmail.com>
2023-08-23 18:55:28 +0100

    solution/m1-027

--- projects/026-is-a-string-a-palindrome/python/main.py
+++ projects/026-is-a-string-a-palindrome/python/main.py
@@ -1,15 +1,1 @@
-while True:
-    integer_number = int(input('Enter an integer number (0 to quit):\n'))
-    if integer_number == 0:
-        break
 
-    sequence = [integer_number]
-    while integer_number != 1:
-        if integer_number % 2 == 0:
-            integer_number = integer_number // 2
-        else:
-            integer_number = integer_number * 3 + 1
-
-        sequence.append(integer_number)
-
-    print(sequence)
