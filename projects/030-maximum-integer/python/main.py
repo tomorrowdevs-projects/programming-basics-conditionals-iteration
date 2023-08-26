dace884bbec89e67025c37c756e508ccb9bcfa3a
Noeddasheep <muceteo@gmail.com>
2023-08-26 08:18:02 +0100

    solution/m1-030

---
+++ projects/030-maximum-integer/python/main.py
@@ -0,0 +1,22 @@
+'''
+Crea un programma che inizi selezionando un numero intero casuale compreso tra 1 e 100.
+Salva questo numero intero come numero massimo incontrato finora
+'''
+import random
+
+maximum_number = random.choice(range(1, 101))
+print(maximum_number, '<== Update')
+i = 1
+
+while maximum_number != 100:
+    new_number = random.choice(range(1, 101))
+
+    if new_number > maximum_number:
+        maximum_number = new_number
+        print(maximum_number, '<== Update')
+        i += 1
+    else:
+        print(new_number)
+
+print(f'The maximum value found was {maximum_number}')
+print(f'The maximum value was updated {i} times')
