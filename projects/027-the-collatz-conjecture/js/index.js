90fff96d713114a355eb47b1aa943d79b994461f
AlessandroVerzella <116301354+AlessandroVerzella@users.noreply.github.com>
2023-08-27 18:50:02 +0200

    enter a first

--- projects/027-the-collatz-conjecture/js/index.js
+++ projects/027-the-collatz-conjecture/js/index.js
@@ -1,1 +1,18 @@
 'use strict';
+
+let num = 0;
+while (true) {
+    let numCollatz = parseFloat(prompt('Enter a positive digit number'));
+
+    if (numCollatz <= 0 || numCollatz === 1) {
+        break;
+    }
+
+    if (numCollatz % 2 === 1) {
+        alert(numCollatz * 3 + 1);
+    } else if (numCollatz % 2 === 0) {
+        alert(numCollatz / 2);
+    } else {
+        console.log('Insert a number');
+    }
+}
