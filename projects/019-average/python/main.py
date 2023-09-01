95f8377a870730db6ac5a70ae0536eb661621b1b
Matteo Muceli <muceteo@gmail.com>
2023-09-01 17:06:01 +0100

    Update main.py

--- projects/019-average/python/main.py
+++ projects/019-average/python/main.py
@@ -1,17 +1,14 @@
-user_list = list(input('Enter your values separated by a comma:\n').split(','))
-int_list = [int(i) for i in user_list]
-
 sum = 0
 counter = 0
-if int_list[0] == 0:
+user_value = int(input('Enter a number (must be different from zero):\n'))
+if user_value == 0:
     print("Error! First value mustn't be zero")
-else:
-    for value in int_list:
-        if value == 0:
-            break
-        else:
-            sum += value
-            counter += 1
+
+while user_value != 0:
+    sum += user_value
+    counter += 1
+    user_value = int(input('Enter a number (must be different from zero):\n'))
+
 
 average = sum / counter
 print(average)
