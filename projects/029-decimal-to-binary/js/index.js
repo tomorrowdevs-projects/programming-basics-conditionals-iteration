b4724525574669330a7773e063aa7e9b9d4eda37
AlessandroVerzella <116301354+AlessandroVerzella@users.noreply.github.com>
2023-09-05 18:36:12 +0200

    added solution and comments

--- projects/029-decimal-to-binary/js/index.js
+++ projects/029-decimal-to-binary/js/index.js
@@ -1,1 +1,16 @@
 'use strict';
+
+let q = parseFloat(prompt('Enter a decimal number')); // Dichiaro una variabile
+let result = '';
+
+while (q > 0) {
+    if (q % 2 === 0) {
+        result = '0' + result; // memorizzo il risultato in result
+    } else {
+        result = '1' + result;
+    }
+    q = Math.floor(q / 2);
+}
+alert(
+    `The binary representation of the decimal number ${q} is as follows: '${result}'`
+);
