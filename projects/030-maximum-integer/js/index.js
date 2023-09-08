ced0546ced79160d8da729107eb5896ed9114009
AlessandroVerzella <116301354+AlessandroVerzella@users.noreply.github.com>
2023-09-08 22:04:19 +0200

    added solution

--- projects/030-maximum-integer/js/index.js
+++ projects/030-maximum-integer/js/index.js
@@ -1,1 +1,17 @@
 'use strict';
+
+let num = Math.floor(Math.random() * 100) + 1;
+let updates = 0;
+console.log(`${num} is the first Maximum Integer`);
+
+for (let i = 0; i < 99; i++) {
+    let i = Math.floor(Math.random() * 100) + 1;
+    console.log(`${i} Update`);
+    if (i > num) {
+        num = i;
+        updates++;
+        console.log(`${i} <== Update`);
+    }
+}
+console.log(`Maximum value encountered: ${num}`);
+console.log(`Number of updates performed: ${updates}`);
