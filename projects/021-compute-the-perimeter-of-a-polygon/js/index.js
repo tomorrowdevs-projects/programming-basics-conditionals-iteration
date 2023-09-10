a2f9e6eda3ff42b3e108771026d15489f753fe12
AlessandroVerzella <116301354+AlessandroVerzella@users.noreply.github.com>
2023-09-10 10:18:40 +0200

    added use strict

--- projects/021-compute-the-perimeter-of-a-polygon/js/index.js
+++ projects/021-compute-the-perimeter-of-a-polygon/js/index.js
@@ -1,1 +1,25 @@
 'use strict';
+
+let firstXCoordinate = parseFloat(
+    prompt(
+        `Enter a number for the x-coordinate. If you don't enter nothing, the programm will stop`
+    )
+);
+let firstYCoordinate = parseFloat(
+    prompt(`Enter a number for the y-coordinate`)
+);
+let nextXCoordinate = firstXCoordinate;
+let nextYCoordinate = firstYCoordinate;
+let counter = 0;
+
+while (true) {
+    let nextXCoordinate = parseFloat(prompt(`Enter a new coordinate X otherwise press enter to end the process`));
+    if nextXCoordinate = " " {
+        break;
+    }
+}
+
+let nextYCoordinate = parseFloat(prompt(`Enter a new coordinate y `));
+ll
+
+
