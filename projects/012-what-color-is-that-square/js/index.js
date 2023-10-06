9845ebe3080af0c01ad21e2e7a7fc271c70639d8
AlessandroVerzella <116301354+AlessandroVerzella@users.noreply.github.com>
2023-10-06 22:30:48 +0200

    Edit the conditions

--- projects/012-what-color-is-that-square/js/index.js
+++ projects/012-what-color-is-that-square/js/index.js
@@ -13,31 +13,23 @@ The letter identifies the column, while the number identifies the row, as shown 
 3) Then use modular arithmetic to report the color of the square in that row. Your program may assume that a valid position will always be entered. It does not need to perform any error checking.*/
 
 // Read input from the user
-const getColumns = prompt('Enter a letter between a and h').toLowerCase();
+const getColumns = prompt('Enter a letter between a and h'.toLowerCase());
 const getRows = parseFloat(prompt('Enter a number between 1 and 8'));
 
 // Create a solution with the remainder. toLowerCase to convert in lower case the getColumns
 if (
-    getColumns === 'a'.toLowerCase() ||
-    getColumns === 'c'.toLowerCase() ||
-    getColumns === 'e'.toLowerCase() ||
-    (getColumns === 'g'.toLowerCase() && getRows % 2 === 1)
+    ((getColumns === 'a' ||
+        getColumns === 'c' ||
+        getColumns === 'e' ||
+        getColumns === 'g') &&
+        getRows % 2 === 1) ||
+    ((getColumns === 'b' ||
+        getColumns === 'd' ||
+        getColumns === 'f' ||
+        getColumns === 'h') &&
+        getRows % 2 === 0)
 ) {
-    alert(`Input = ${getColumns}${getRows}; Output = black`);
-} else if (
-    getColumns === 'b'.toLowerCase() ||
-    getColumns === 'd'.toLowerCase() ||
-    getColumns === 'f'.toLowerCase() ||
-    (getColumns === 'h'.toLowerCase() && getRows % 2 === 0)
-) {
-    alert(`Input = ${getColumns}${getRows}; Output = black`);
-} else if (
-    getColumns === 'a'.toLowerCase() ||
-    getColumns === 'c'.toLowerCase() ||
-    getColumns === 'e'.toLowerCase() ||
-    (getColumns === 'g'.toLowerCase() && getRows % 2 === 0)
-) {
-    alert(`Input = ${getColumns}${getRows}; Output = white`);
+    alert(`Input = ${getColumns.toUpperCase()}${getRows}; Output = black`);
 } else {
-    alert(`Input = ${getColumns}${getRows}; Output = white`);
+    alert(`Input = ${getColumns.toUpperCase()}${getRows}; Output = white`);
 }
