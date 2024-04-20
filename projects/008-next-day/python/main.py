4116e79ebf12fcd4c431f96195178e9c266eca70
Roberto <robertomoggia95@gmail.com>
2024-04-20 21:56:07 +0200

    solution/m1-008
    Second commit - Fixed bug in code

--- projects/008-next-day/python/main.py
+++ projects/008-next-day/python/main.py
@@ -2,34 +2,59 @@ year=int(input('Please, enter the year your interested in:'))
 month=int(input('Please, enter the month your interested in:'))
 day=int(input('Please, enter the day your interested in:'))
 
-if day==28 and month==2: #check if the year is leap
+if month==2:
     if year%400==0: #leap year
-        next_day=29
-        next_month=2
-        next_year=year
+        leap_year=True
     elif year%100==0: #not leap year
-        next_day=1
-        next_month=3
-        next_year=year
+        leap_year=False
     elif year%4==0: #leap year
-        next_day=29
-        next_month=2
-        next_year=year
+        leap_year=True
     else: #not leap year
-        next_day=1
-        next_month=3
+        leap_year=False
+    
+    if leap_year:
+        if day>29:
+            print('Plese enter an existing date')
+        elif day<29:
+            next_day=day+1
+            next_month=month
+            next_year=year
+        else:
+            next_day=1
+            next_month=month+1
+            next_year=year
+    else:
+        if day>28:
+            print('Plese enter an existing date')
+        elif day<28:
+            next_day=day+1
+            next_month=month
+            next_year=year
+        else:
+            next_day=1
+            next_month=month+1
+            next_year=year
+elif month==11 or month==4 or month==6 or month==9:
+    if day>30:
+        print('Plese enter an existing date')
+    elif day<30:
+        next_day=day+1
+        next_month=month
         next_year=year
-elif (str(month) in '11-4-6-9' and day==30) or (str(month) in '1-3-5-7-8-10' and day==31): #check if month must be changed
-    next_day=1
-    next_month=month+1
-    next_year=year
-elif month==12 and day==31: #check if year must be changed
-    next_day=1
-    next_month=1
-    next_year=year+1
-else: #generic case
-    next_day=day+1
-    next_month=month
-    next_year=year
+    else:
+        next_day=1
+        next_month=month+1
+        next_year=year
+else:
+    if day>31:
+        print('Plese enter an existing date')
+    elif day<31:
+        next_day=day+1
+        next_month=month
+        next_year=year
+    else:
+        next_day=1
+        next_month=month+1
+        next_year=year
 
 print('{}-{:02d}-{:02d}'.format(next_year,next_month,next_day))
