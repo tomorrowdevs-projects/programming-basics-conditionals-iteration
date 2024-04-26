802d3bce1e9c092977887798c83e75c71fd46fb7
Roberto <robertomoggia95@gmail.com>
2024-04-26 17:54:58 +0200

    solution/m1-017
    First commit

---
+++ projects/017-caesar-cipher/python/main.py
@@ -0,0 +1,37 @@
+message=input("Please, enter the message you want to cipher:")
+#CHECK FOR CORRECT INPUT#
+#Uncomment line 4 and comment lines from 5 to 11 to pass the test (mock input pass an int value so removeprefix rise an error)
+#shift=input("Please, enter the shift factor:")
+while True:
+    shift=input("Please, enter the shift factor:")
+    if shift.removeprefix('-').isdigit():
+        shift=int(shift)
+        break
+    else:
+        print("Please, enter a numeric shift (or press ctrl+c to quit):")
+
+index=0
+new_message=""
+while index<=len(message)-1:
+    char=message[index]
+    if char.isalpha():
+        char=ord(char)
+        if 65<=char<=90:
+            if char+shift>90:
+                char=65+(char+shift-90)-1
+            elif char+shift<65:
+                char=90+(char+shift-65)+1
+            else:
+                char=char+shift
+        else:
+            if char+shift>122:
+                char=97+(char+shift-122)-1
+            elif char+shift<97:
+                char=122+(char+shift-97)+1
+            else:
+                char=char+shift
+        new_message=new_message+chr(char)
+    else:
+        new_message=new_message+char
+    index+=1
+print(new_message)
