917646ccc6c0ba2c3b00460f8180b4ea669bad82
Roberto <robertomoggia95@gmail.com>
2024-05-06 22:49:33 +0200

    solution/m1-011
    Second commit

--- projects/011-average/python/main.py
+++ projects/011-average/python/main.py
@@ -1,17 +1,14 @@
-counter=1
-number=float(input('Please keep iputing the numbers whose mean you are interested in and input 0 when you have finished:'))
-if number==0:
-    print('Please type at least one number before typing 0')
-else:
-    sum=number
-    while True:
-        number=float(input('Please keep inputing the numbers whose mean you are interested in and input 0 when you have finished:'))
+counter=0
+sum=0
+while True:
+    number=float(input('Please keep iputing the numbers whose mean you are interested in and input 0 when you have finished:'))
+    if counter==1 and number==0:
+        print('Please type at least one number before typing 0')
+    else:
         if number==0:
             break
         else:
             sum+=number
             counter+=1
-    mean=round(sum/counter,2)
-    print('Mean = {}'.format(mean))
-
-
+mean=round(sum/counter,2)
+print('Mean = {}'.format(mean))
