11ec61e06622a468858cb665f0e53bb3faa2b9ef
AlessandroVerzella <alessandro.verzella@web.de>
2024-05-06 18:47:52 +0200

    Added some modifications to the exercise

--- projects/028-prime-factors/js/index.js
+++ projects/028-prime-factors/js/index.js
@@ -1,18 +1,17 @@
 'use strict';
 
-let number = parseFloat(prompt('Enter an integer (2 or greater)'));
-let fattore = 2;
+let userInput = Number(prompt('Enter an integer (2 or greater)'));
+let factor = 2;
 
-while (number < 2) {
-    console.log('Insert a number greater or equal than 2');
-    break;
+while (userInput < 2 || isNaN(userInput)) {
+    console.log('Insert an integer greater or equal than 2');
+    userInput = Number(prompt('Enter an integer (2 or greater)'));
 }
-while (number > 1) {
-    if (number % fattore === 0) {
-        console.log(fattore);
-        number = number / fattore;
+while (userInput > 1) {
+    if (userInput % factor === 0) {
+        console.log(factor);
+        userInput = userInput / factor;
     } else {
-        fattore++;
+        factor++;
     }
-    console.log(`For the ${number} ci sono i seguenti numeri: ${fattore};`);
 }
