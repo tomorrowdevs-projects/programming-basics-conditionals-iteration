e40402f3c048147722a8252adb0b26712f20cc35
Roberto <robertomoggia95@gmail.com>
2024-05-06 23:25:17 +0200

    solution/m1-013
    Second commit

--- projects/013-compute-the-perimeter-of-a-polygon/python/main.py
+++ projects/013-compute-the-perimeter-of-a-polygon/python/main.py
@@ -1,36 +1,40 @@
 while True:
     x0=input('Please enter the x coordinate for the first point of the polygon:')
-    if not x0.isnumeric():
+    if x0.count('.')>1 and not x0.lstrip('.').isdecimal():
         print('Please, enter a numeric value or presse ctrl+c to quit the app')
     else:
-        x0=int(x0)
+        x0=float(x0)
         break
 
 while True:
     y0=input('Please enter the y coordinate for the first point of the polygon:')
-    if not y0.isnumeric():
+    if y0.count('.')>1 and not y0.lstrip('.').isdecimal():
         print('Please, enter a numeric value or presse ctrl+c to quit the app')
     else:
-        y0=int(y0)
+        y0=float(y0)
         break
 
 perimiter=0
-exit_flag=True
-while exit_flag:
+while True:
     x=input('Please enter the x coordinate for the next point of the polygon (blanck to quit the app):')
     if len(x)>0:
-        x=int(x)
+        if x.count('.')>1 and not x.lstrip('.').isdecimal():
+            print('Please, enter a numeric value or presse ctrl+c to quit the app')
+            continue
+        else:
+            x=float(x)
+
         while True:
             y=input('Please enter the y coordinate for the next point of the polygon:')
-            if not y.isnumeric():
+            if y.count('.')>1 and not y.lstrip('.').isdecimal():
                 print('Please, enter a numeric value or presse ctrl+c to quit the app')
             else:
-                y=int(y)
+                y=float(y)
                 break
         perimiter+=((x0-x)**2+(y0-y)**2)**0.5
         x0=x
         y0=y
     else:
-        exit_flag=False
+        break
 
 print('The perimiter of that polygon is {}'.format(round(perimiter,2)))
