e056176044c070c2555a2c2ff1ca4dbd7ba49b21
Stefano Agnelli <stefano.agnelli.jobs@gmail.com>
2024-06-25 01:17:08 +0200

    solution m2-007

---
+++ projects/007-is-it-a-leap-year/python/main.py
@@ -0,0 +1,19 @@
+
+u_year = input('Insert a year: ')
+u_year = int(u_year)
+
+if u_year % 400 == 0:
+    print('Leap year')
+
+elif u_year % 100 == 0:
+    pass
+
+elif u_year % 4 == 0:
+    print('Leap year')
+
+else:
+    pass
+
+
+
+ 
