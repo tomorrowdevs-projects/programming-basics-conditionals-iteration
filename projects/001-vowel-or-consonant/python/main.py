5da29a516f935845c2ab41c38132dee681f43f57
Natalia <natalia.muccio@gmail.com>
2024-07-17 19:51:46 +0200

    solution/m1-001

---
+++ projects/001-vowel-or-consonant/python/main.py
@@ -0,0 +1,8 @@
+letter=input("Enter a letter:")
+
+if letter=="a" or letter=="e" or letter=="i" or letter=="o" or letter=="u":
+    print("Entered letter is a vowel")
+elif letter=="y":
+    print("Sometimes y is a vowel and sometimes y is a consonant")    
+else:
+    print("Entered letter is a consonant")
