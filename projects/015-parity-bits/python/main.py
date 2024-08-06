08ac09f642c88837ad416d2348faf6bda74cfe8a
Stefano Agnelli <stefano.agnelli.jobs@gmail.com>
2024-08-06 22:16:55 +0200

    solution m2-015 fix

--- projects/015-parity-bits/python/main.py
+++ projects/015-parity-bits/python/main.py
@@ -23,24 +23,51 @@
 
 
 
+# byte = []
+# x = 0
+# u_bit = None
+
+# while x < 8 and u_bit != '':
+#     x = x + 1
+#     u_bit = input(f'Insert the bit number {x}: ')
+#     byte.append(u_bit)
+    
+    
+# if x <= 8 and u_bit == '':
+#     print('The number of bits must be equal to 8')
+
+# else:
+#     n = byte.count('1')
+    
+#     if n % 2 == 0:
+#         print('The parity bit is: 0')
+        
+#     else:
+#         print('The parity bit is: 1')
+
+
 byte = []
-x = 0
-u_bit = 'none'
+counter = 0
+is_error = False
 
-while x < 8 and u_bit != '':
-    x = x + 1
+for x in range(8):
     u_bit = input(f'Insert the bit number {x}: ')
-    byte.append(u_bit)
-    
-    
-if x <= 8 and u_bit == '':
-    print('The number of bits must be equal to 8')
+    if u_bit == '':
+        print('The number of bits must be equal to 8')
+        is_error = True
+        break
 
-else:
-    n = byte.count('1')
+    elif u_bit == '1':
+        counter += 1
     
-    if n % 2 == 0:
+    byte.append(u_bit)
+
+if not is_error:
+    if counter % 2 == 0:
         print('The parity bit is: 0')
         
     else:
         print('The parity bit is: 1')
+
+
+
