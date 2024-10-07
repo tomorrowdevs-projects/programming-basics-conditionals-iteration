1c7fb7616f4d6b2f356239425d9a4196843603b5
Natalia <natalia.muccio@gmail.com>
2024-10-08 00:19:07 +0200

    solution/m1-018

--- projects/018-is-a-string-a-palindrome/python/main.py
+++ projects/018-is-a-string-a-palindrome/python/main.py
@@ -1,13 +1,15 @@
-string=input("Enter a string: ")
-i=0
-j=len(string)-1
+string_origin=input("Enter a string: ")
+string=string_origin.lower()
+i=1
+half_lenght=len(string)//2
 
-while i<j:
-    if string.lower()[i]!=string.lower()[j]:
-        result="is not a palindrome"
-    else:
-        result="is a palindrome"
+is_palindrome=True
+while is_palindrome and i<=half_lenght:
+    if string[i-1]!=string[-i]:
+        is_palindrome=False
     i+=1
-    j-=1
 
-print(f"{string} {result}")
+if is_palindrome:
+    print(f"{string_origin} is a palindrome")
+else:
+    print(f"{string_origin} is not a palindrome")
